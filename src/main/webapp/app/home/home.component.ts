005b54bbe887750884f1277dce18f0b667c34730
TienDung1605 <TienDung1605>
2024-07-06 08:23:56 +0700

    update 06072024

--- src/main/webapp/app/home/home.component.ts
+++ src/main/webapp/app/home/home.component.ts
@@ -16,6 +16,7 @@ export class HomeComponent implements OnInit {
 
   ngOnInit(): void {
     this.accountService.identity().subscribe(account => (this.account = account));
+    // this.login()
   }
 
   login(): void {
@@ -23,50 +24,50 @@ export class HomeComponent implements OnInit {
   }
 
   linkMRP(): void {
-    window.open('http://192.168.68.96:4200/', "_blank")
+    window.open('http://192.168.68.96:4200/', '_blank');
   }
 
   linkPlanning(): void {
-    window.open('http://192.168.68.91:3000/', "_blank")
+    window.open('http://192.168.68.91:3000/', '_blank');
   }
 
   linkDOC(): void {
-    window.open('http://192.168.68.88', "_blank")
+    window.open('http://192.168.68.88', '_blank');
   }
 
   linkForum(): void {
-    window.open('http://192.168.68.94:8800/', "_blank")
+    window.open('http://192.168.68.94:8800/', '_blank');
   }
 
   linkWarning(): void {
-    window.open('http://192.168.68.94:9080/', "_blank")
+    window.open('http://192.168.68.94:9080/', '_blank');
   }
 
   linkReport(): void {
-    window.open('http://192.168.68.86:4301/', "_blank")
+    window.open('http://192.168.68.86:4301/', '_blank');
   }
 
   linkWMS(): void {
-    window.open('http://172.168.6.3/PanaCIMMC', "_blank")
+    window.open('http://172.168.6.3/PanaCIMMC', '_blank');
   }
 
   linkR2(): void {
-    window.open('http://192.168.68.91:3000/', "_blank")
+    window.open('http://192.168.68.91:3000/', '_blank');
   }
 
   linkQMS(): void {
-    window.open('http://192.168.68.92/', "_blank")
+    window.open('http://192.168.68.92/', '_blank');
   }
 
   linkSCADA(): void {
-    window.open('http://192.168.68.95:4200/', "_blank")
+    window.open('http://192.168.68.95:4200/', '_blank');
   }
 
   linkMaintance(): void {
-    window.open('http://192.168.68.77:8123/', "_blank")
+    window.open('http://192.168.68.77:8123/', '_blank');
   }
 
   linkDoiTra(): void {
-    window.open('http://192.168.68.77:8086/', "_blank")
+    window.open('http://192.168.68.77:8086/', '_blank');
   }
 }
